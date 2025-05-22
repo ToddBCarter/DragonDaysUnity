4626258b02cb87ab9d4895cd16f1be79656dc8ba
tCarter <tcart00990@gmail.com>
2025-05-22 15:16:29 -0700

    Plank placement progress

--- Assets/Scripts/PlayerBuild.cs
+++ Assets/Scripts/PlayerBuild.cs
@@ -1,26 +1,38 @@
 using UnityEngine;
+using TMPro;
 
 public class PlayerBuild : MonoBehaviour
 {
-    public GameObject objectToPlacePrefab; //Assign in Inspector
+    public GameObject objectToPlacePrefab; //Assign in Inspector; this should change to buttons on menu
     public float gridSize = 1f;
 	
 	private void Update()
-	{
+	{		
 		if (Input.GetMouseButtonDown(0)) //Left click
         {
             Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 gridPos = SnapToGrid(mouseWorldPos);
+			Vector2 objectSize = GetPrefabSize(objectToPlacePrefab);
+            Vector2 gridPos = SnapToGrid(mouseWorldPos, objectSize);
 
             //Instantiate the object at the snapped grid position
-            Instantiate(objectToPlacePrefab, gridPos, Quaternion.identity);
+            Instantiate(objectToPlacePrefab, gridPos, Quaternion.identity);			
         }
 	}
 	
-    Vector2 SnapToGrid(Vector2 rawWorldPos)
+	Vector2 GetPrefabSize(GameObject prefab)
+	{
+		SpriteRenderer sr = prefab.GetComponent<SpriteRenderer>();
+		if (sr != null)
+		{
+			return sr.bounds.size;
+		}
+		return Vector2.one; //Default if no renderer
+	}
+	
+    Vector2 SnapToGrid(Vector2 rawWorldPos, Vector2 size)
     {
-        float x = Mathf.Round(rawWorldPos.x / gridSize) * gridSize;
-        float y = Mathf.Round(rawWorldPos.y / gridSize) * gridSize;
+        float x = Mathf.Round(rawWorldPos.x / size.x) * size.x;
+        float y = Mathf.Round(rawWorldPos.y / size.y) * size.y;
         return new Vector2(x, y);
     }	
 
